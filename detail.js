ae8952cd24743dfabada0f15dde6c30e3683c096
DivadanAr <divadan.arya12@gmail.com>
2022-11-09 11:01:31 +0700

    modal preview detail

--- detail.js
+++ detail.js
@@ -1,75 +1,27 @@
-// other product
+// preview
+const set = document.querySelector('.other-product');
+set.addEventListener('click', handleClick, false);
 
-var product1 = document.getElementById("product1").src;
-var product2 = document.getElementById("product2").src;
-var product3 = document.getElementById("product3").src;
-var product4 = document.getElementById("product4").src;
-var product5 = document.getElementById("product5").src;
-
-var mainProd1 = document.getElementById("product1");
-var mainProd2 = document.getElementById("product2");
-var mainProd3 = document.getElementById("product3");
-var mainProd4 = document.getElementById("product4");
-var mainProd5 = document.getElementById("product5");
-
-mainProd2.onclick = function() {
-  mainProd1.src = product2;
+function handleClick(e) {  
+  if (e.target.matches('img')) {
+    var preview = e.target.src;
+    document.getElementById("main-product").src = preview;
+  }
 }
-
-mainProd3.onclick = function() {
-  mainProd1.src = product3;
-}
-
-mainProd4.onclick = function() {
-  mainProd1.src = product4;
-}
-
-mainProd5.onclick = function() {
-  mainProd1.src = product5;
-}
-
-
-var modal1 = document.getElementById("modal1").src;
-var modal2 = document.getElementById("modal2").src;
-var modal3 = document.getElementById("modal3").src;
-var modal4 = document.getElementById("modal4").src;
-var modal5 = document.getElementById("modal5").src;
-
-var mainModal1 = document.getElementById("modal1");
-var mainModal2 = document.getElementById("modal2");
-var mainModal3 = document.getElementById("modal3");
-var mainModal4 = document.getElementById("modal4");
-var mainModal5 = document.getElementById("modal5");
-
-mainModal2.onclick = function() {
-  mainModal1.src = modal2;
-}
-
-mainModal3.onclick = function() {
-  mainModal1.src = modal3;
-}
-
-mainModal4.onclick = function() {
-  mainModal1.src = modal4;
-}
-
-mainModal5.onclick = function() {
-  mainModal1.src = modal5;
-}
-
 
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("product1");
+var btn = document.getElementById("main-product");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
+function prev(element) {
   modal.style.display = "block";
+  document.getElementById("main-modal").src = element.src;
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -83,3 +35,87 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 }
+
+var modalRev = document.getElementById("myRev");
+
+// Get the button that opens the modal
+var btnRev = document.getElementById("btn-review");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-rev")[0];
+
+// When the user clicks the button, open the modal 
+btnRev.onclick = function () {
+  modalRev.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modalRev.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalRev) {
+    modalRev.style.display = "none";
+  }
+}
+
+const setModal = document.querySelector('.other-modal');
+setModal.addEventListener('click', onTheClick, false);
+
+function onTheClick(e) {  
+  if (e.target.matches('img')) {
+    var preview = e.target.src;
+    document.getElementById("main-modal").src = preview;
+  }
+}
+
+// var img = [
+//   document.querySelector('.other-modal img:nth-of-type(1)').src,
+//   document.querySelector('.other-modal img:nth-of-type(2)').src,
+//   document.querySelector('.other-modal img:nth-of-type(3)').src,
+//   document.querySelector('.other-modal img:nth-of-type(4)').src,
+// ]
+
+// let imageTag = document.querySelector('#main-modal');
+// let i = 0;
+
+// prevBtn = document.querySelector(".modal-prev");
+// nextBtn = document.querySelector(".modal-next");
+
+
+// nextBtn.addEventListener("click", function () {
+//   if (i >= img.length - 1) {
+//       return false;
+//   }
+//   i++;
+//   imageTag.setAttribute('src', img[i]);
+// });
+
+// prevBtn.addEventListener("click", function () {
+//   if (i <= 0) {
+//       return false;
+//   }
+//   i--;
+//   imageTag.setAttribute('src', img[i]);
+// });
+
+
+// var mainImg = document.querySelector("#main-modal");
+// var allModalImgs = document.querySelectorAll(".other-modal img");
+
+// prevBtn = document.querySelector(".modal-prev");
+// nextBtn = document.querySelector(".modal-next");
+
+// var currentImg = 0;
+
+// nextBtn.addEventListener("click", function () {
+//   mainImg.src = allModalImgs[currentImg].src;
+//   currentImg = (currentImg += 1) % allModalImgs.length
+// });
+
+// prevBtn.addEventListener("click", function () {
+//   mainImg.src = allModalImgs[currentImg].src;
+//   currentImg = (currentImg -= 1) % allModalImgs.length
+// });
